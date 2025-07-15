138754fe78215c013b832f99bd5394668af75158
Jirasak-Thongpire <tle0613828245@gmail.com>
2025-07-15 09:13:10 +0700

    Refactor build script for clarity and enable database configuration in settings

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+echo "BUILD START"
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
+echo "BUILD END"
