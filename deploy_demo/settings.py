d96d8e70cbedc849c730d035cd887cc8222b85c9
Jirasak-Thongpire <tle0613828245@gmail.com>
2025-07-15 09:01:15 +0700

    make templates,config vercel

--- deploy_demo/settings.py
+++ deploy_demo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webpage',  # Custom app for the project
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'deploy_demo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],  # Directory for custom templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
